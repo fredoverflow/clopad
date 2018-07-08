91806385afe4815e4cfe31192cc21d48af9df787
fredoverflow <fredoverflow@users.noreply.github.com>
2018-07-08 12:18:26 +0200

    improve symbol flexing

--- src/main/java/FlexerGenerator.java
+++ src/main/java/FlexerGenerator.java
@@ -1,7 +1,7 @@
 public class FlexerGenerator {
     public static void main(String[] args) {
         new freditor.FlexerGenerator(-10, 11).generateTokens(
-                "#", "#?", "#?@", "%", "'", "(", ")", "<", "<=", "=", "==", ">", ">=", "@", "[", "]", "^", "`", "{", "}", "~", "~@"
+                "(", ")", "[", "]", "{", "}"
         );
     }
 }
