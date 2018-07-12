516653921943ce97b626856525242a2811a8a6be
fredoverflow <fredoverflow@users.noreply.github.com>
2018-07-12 20:33:10 +0200

    flex signed numbers

--- src/main/java/FlexerGenerator.java
+++ src/main/java/FlexerGenerator.java
@@ -1,6 +1,6 @@
 public class FlexerGenerator {
     public static void main(String[] args) {
-        new freditor.FlexerGenerator(-8, 9)
+        new freditor.FlexerGenerator(-9, 9)
                 .withIdentifierCall("symbol(input)")
                 .generateTokens(
                         "false", "nil", "true",
