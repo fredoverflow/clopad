1c851c5b253b85ade452fdab521e5af4f3628b95
fredoverflow <fredoverflow@users.noreply.github.com>
2018-08-17 08:19:15 +0200

    SwingConfig

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import clojure.lang.RT;
+import freditor.SwingConfig;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        SwingConfig.metalWithDefaultFont(SwingConfig.SANS_SERIF_PLAIN_16);
         SwingUtilities.invokeLater(MainFrame::new);
-
         RT.init();
     }
 }
